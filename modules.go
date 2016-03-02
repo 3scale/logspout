b3d6eb4279a1d45932cef3aac69e6ffa2d1237e0
Oriol Martí <orimarti@gmail.com>
2016-03-02 12:50:26 +0100

    Changed modules to use the 3scale logspout repos

--- modules.go
+++ modules.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	_ "github.com/gliderlabs/logspout/adapters/raw"
-	_ "github.com/gliderlabs/logspout/adapters/syslog"
-	_ "github.com/gliderlabs/logspout/httpstream"
-	_ "github.com/gliderlabs/logspout/routesapi"
-	_ "github.com/gliderlabs/logspout/transports/tcp"
-	_ "github.com/gliderlabs/logspout/transports/udp"
-	_ "github.com/gliderlabs/logspout/transports/tls"
+	_ "github.com/3scale/logspout/adapters/raw"
+	_ "github.com/3scale/logspout/adapters/syslog"
+	_ "github.com/3scale/logspout/httpstream"
+	_ "github.com/3scale/logspout/routesapi"
+	_ "github.com/3scale/logspout/transports/tcp"
+	_ "github.com/3scale/logspout/transports/udp"
+	_ "github.com/3scale/logspout/transports/tls"
+	_ "github.com/3scale/logspout-redis-logstash"
 )
